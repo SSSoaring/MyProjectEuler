e8468742ada27f6f3f7470141b9af394f2ee0e17
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-01-14 20:56:51 +0800

    Create 021.cpp

---
+++ src/021.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int compute_d(int n)
+{
+    int sum=1;
+    for(int i=2;i<=sqrt(n);++i)
+    {
+        if(n%i==0) sum+=i+n/i;
+        if(i*i==n) sum-=i;
+    }
+    return sum;
+}
+
+int main()
+{
+    int d[30000];
+    for(int i=1;i<30000;++i) d[i]=0;
+    for(int i=1;i<10000;++i)
+    {
+        d[i]=compute_d(i);
+        if(d[i]>9999) d[d[i]]=compute_d(d[i]);
+    }
+    int sum=0;
+    for(int i=1;i<10000;++i)
+    {
+        if(d[d[i]]==i&&d[i]!=i)
+        {
+            sum+=i;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
