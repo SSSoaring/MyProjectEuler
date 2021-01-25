a5907debf1aca744c0cd7802652159cd60cb679d
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-01-25 18:32:42 +0800

    Update 037.cpp

--- src/037.cpp
+++ src/037.cpp
@@ -8,6 +8,57 @@ bool p[MAX];
 
 int main()
 {
+    for(int i=3; i<MAX; ++i)
+        p[i]=i%2;
+    p[1]=false;
+    p[2]=true;
+    for(int i=3; i<MAX; ++i)
+    {
+        if(!p[i])
+            continue;
+        for(int j=i*3; j<MAX; j+=i*2)
+        {
+            p[j]=false;
+        }
+    }
+    int sum=0,tmp,k=1;
+    bool flag;
+
+    for(int i=11;i<MAX;i+=2)
+    {
+        if(!p[i]) continue;
+        tmp=i/10;
+        flag=true;
+        while(tmp!=0)
+        {
+            if(!p[tmp])
+            {
+                flag=false;
+                break;
+            }
+            tmp/=10;
+        }
+        if(!flag) continue;
+        tmp=i;
+        k=10;
+        while(tmp%k!=tmp)
+        {
+            if(!p[tmp%k])
+            {
+                flag=false;
+                break;
+            }
+            k*=10;
+        }
+        if(!flag) continue;
+        sum+=i;
+    }
+    cout<<sum;
+    return 0;
+}
+
+int anothermethod()
+{
     for(int i=2; i<MAX; ++i)
         p[i]=i%2;
     p[2]=true;
@@ -31,7 +82,6 @@ int main()
             if(p[10*p1[a]+p1[b]])
             {
                 sum+=10*p1[a]+p1[b];
-                cout<<p1[a]<<p1[b]<<endl;
             }
         }
     }
@@ -45,7 +95,6 @@ int main()
                 if(p[100*p1[a]+10*b+p1[c]]&&p[10*b+p1[c]]&&p[10*p1[a]+b])
                 {
                     sum+=100*p1[a]+10*b+p1[c];
-                    cout<<p1[a]<<b<<p1[c]<<endl;
                 }
             }
         }
@@ -63,7 +112,6 @@ int main()
                        &&p[10*c+p1[d]]&&p[100*p1[a]+10*b+c]&&p[10*p1[a]+b])
                     {
                         sum+=1000*p1[a]+100*b+10*c+p1[d];
-                        cout<<p1[a]<<b<<c<<p1[d]<<endl;
                     }
                 }
             }
@@ -85,7 +133,6 @@ int main()
                            &&p[1000*p1[a]+100*b+10*c+d]&&p[100*p1[a]+10*b+c]&&p[10*p1[a]+b])
                         {
                             sum+=10000*p1[a]+1000*b+100*c+10*d+p1[e];
-                            cout<<p1[a]<<b<<c<<d<<p1[e]<<endl;
                         }
                     }
                 }
@@ -110,7 +157,6 @@ int main()
                                     &&p[10000*p1[a]+1000*b+100*c+10*d+e]&&p[1000*p1[a]+100*b+10*c+d]&&p[100*p1[a]+10*b+c])
                             {
                                 sum+=100000*p1[a]+10000*b+1000*c+100*d+10*e+p1[f];
-                                cout<<p1[a]<<b<<c<<d<<e<<p1[f]<<endl;
                             }
                         }
                     }
@@ -121,3 +167,4 @@ int main()
     cout<<sum;
     return 0;
 }
+
