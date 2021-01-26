f481f8931dd9f34e5c9cbe551e8111d54effc1cc
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-01-26 14:28:06 +0800

    Update 043.cpp

--- src/043.cpp
+++ src/043.cpp
@@ -16,6 +16,7 @@ int main()
 {
     int num[10];
     long long int tmp,sum=0;
+    num[5]=5;
     for(num[9]=0;num[9]<10;++num[9])
     {
         for(num[8]=0;num[8]<10;++num[8])
@@ -27,10 +28,9 @@ int main()
                 if((num[7]*100+num[8]*10+num[9])%17!=0) continue;
                 for(num[6]=0;num[6]<10;++num[6])
                 {
+                    if((num[5]+num[7]-num[6])%11!=0) continue;
                     if(!check(num,6)) continue;
                     if((num[6]*100+num[7]*10+num[8])%13!=0) continue;
-                    num[5]=5;
-                    if((num[5]*100+num[6]*10+num[7])%11!=0) continue;
                     for(num[4]=0;num[4]<10;++num[4])
                     {
                         if(!check(num,4)) continue;
@@ -41,7 +41,7 @@ int main()
                             for(num[2]=0;num[2]<10;++num[2])
                             {
                                 if(!check(num,2)) continue;
-                                if((num[2]*100+num[3]*10+num[4])%3!=0) continue;
+                                if((num[2]+num[3]+num[4])%3!=0) continue;
                                 for(num[1]=0;num[1]<10;++num[1])
                                 {
                                     if(!check(num,1)) continue;
