f849b5a7ecec7518df40ae0de0d1b797f934a38c
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-01-30 14:59:21 +0800

    Update 058.cpp

--- src/058.cpp
+++ src/058.cpp
@@ -1,45 +1,38 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-const long long int MAX=1000000000;
-bool p[MAX];
+bool is_prime(int n)
+{
+	int limit=sqrt(n);
+	if(n%3==0) return false;
+	for(int i=5; i <= limit; i+=6)
+    {
+        if(n%i==0) return false;
+        if(n%(i+2)==0) return false;
+    }
+	return true;
+}
 
 int main()
 {
-    for(long long int i=3; i<MAX; ++i)
-        p[i]=i%2;
-    p[1]=false;
-    p[2]=true;
-    for(long long int i=3;i<MAX;++i)
-    {
-        if(!p[i])
-            continue;
-        for(long long int j=i*3;j<MAX;j+=i*2)
-        {
-            p[j]=false;
-        }
-    }
-
-    int i=1;
-    long long int n;
-    int cnt=0;
-    double ratio;
+    int i=3;
+    int n=3;
+    int cnt=3;
     while(true)
     {
-        ++i;
-        n=4*i*i-4*i+1;
-        if(p[n]) ++cnt;
-        n=4*i*i-6*i+3;
-        if(p[n]) ++cnt;
-        n=4*i*i-8*i+5;
-        if(p[n]) ++cnt;
-        n=4*i*i-10*i+7;
-        if(p[n]) ++cnt;
-        ratio=cnt/(double)(4*i-3);
-        //cout<<ratio<<endl;
-        if(ratio<0.1) break;
+        i+=2;
+        n=n+7*i-13;
+        if(is_prime(n)) ++cnt;
+        n=n-i+1;
+        if(is_prime(n)) ++cnt;
+        n=n-i+1;
+        if(is_prime(n)) ++cnt;
+        n=n-i+1;
+        if(is_prime(n)) ++cnt;
+        if(cnt/(double)(2*i-1)<0.1) break;
     }
-    cout<<2*i-1;
+    cout<<i;
     return 0;
 }
