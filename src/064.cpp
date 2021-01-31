e2a78566a0596213ecc5b1e581686f95b6375644
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-01-31 21:09:04 +0800

    Update 064.cpp

--- src/064.cpp
+++ src/064.cpp
@@ -17,14 +17,14 @@ private:
     int b;
     int den;
 public:
-    fraction(int n)
+    void set(int n,int a0)
     {
         N=n;
         k=1;
-        b=0;
-        den=1;
+        b=a0;
+        den=n-a0*a0;
     }
-    bool iter()
+    int iter()
     {
         int a=(k*sqrt(N)+b)/den;
         int k_new=den*k;
@@ -34,36 +34,27 @@ public:
         k=k_new/factor;
         b=b_new/factor;
         den=den_new/factor;
-        return den==0;
-    }
-    bool operator==(const fraction &f)
-    {
-        return (k==f.k)&&(N==f.N)&&(b==f.b)&&(den==f.den);
+        return a;
     }
 };
 
-bool check(int n)
-{
-    fraction f(n),tmp(n);
-    if(f.iter()) return false;
-    tmp=f;
-    int i=1;
-    while(true)
-    {
-        f.iter();
-        if(f==tmp) break;
-        ++i;
-    }
-    return (i%2==1);
-}
-
-
 int main()
 {
-    int cnt=0;
+    int cnt=0,period,a0,ai;
+    fraction f;
     for(int i=1;i<10000;++i)
     {
-        if(check(i)) ++cnt;;
+        a0=sqrt(i);
+        if(a0*a0==i) continue;
+        f.set(i,a0);
+        period=1;
+        while(true)
+        {
+            ai=f.iter();
+            if(ai==2*a0) break;
+            ++period;
+        }
+        if (period%2==1) ++cnt;
     }
     cout<<cnt;
     return 0;
