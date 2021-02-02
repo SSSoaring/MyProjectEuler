1fbaa2d860cf1880eb029365729eecf968ca7982
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-02-02 23:31:35 +0800

    Update 070.cpp

--- src/070.cpp
+++ src/070.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -10,27 +11,34 @@ int p[MAX];
 
 int main()
 {
-    for(int i=3; i<MAX; ++i)
+    for(int i=1; i<MAX; ++i)
     {
-        if(i%2==1) p[i]=i;
-        else p[i]=i/2;
+        if(i%2==0) p[i]=2;
+        if(i%4==0) p[i]=0;
+        else p[i]=1;
     }
-    p[1]=1;
-    p[2]=1;
-    for(int i=3;i<MAX;++i)
+    for(int i=3;i<MAX;i+=2)
     {
-        if(p[i]!=i) continue;
-        for(int j=i;j<MAX;j+=i)
+        if(p[i]!=1) continue;
+        p[i]=0;
+        for(int j=2*i;j<MAX;j+=i)
         {
-            p[j]=p[j]/i*(i-1);
+            if(p[j]==1) p[j]=i;
+            else
+            {
+                if(p[j]*i!=j) p[j]=0;
+                else p[j]=(p[j]-1)*(i-1);
+            }
         }
     }
 
     string n,phi_n;
-    int min_i=0;
+    int min_i=0,limit=sqrt(MAX);
     double min_ratio=2,ratio;
-    for(int i=2;i<MAX;++i)
+
+    for(int i=MAX-1;i>=limit;i-=2)
     {
+        if(p[i]==0) continue;
         ratio=i/(double)p[i];
         if(ratio>min_ratio) continue;
         n=to_string(i);
